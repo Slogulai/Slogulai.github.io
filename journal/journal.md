30259ae0f11c7069d692b70eb40dff91352cf5e2
Christopher Sloggett <mrsloggett@gmail.com>
2024-11-24 14:25:03 -0800

    Navbar is now done with use of JS. The home page has also been created as a welcome page in index.html. Journal has been updated with some references that I used to acheive the navbar style and JS. The intention here is that the navbar will be loaded onto each page while only needing to code the html once

---
+++ journal/journal.md
@@ -0,0 +1,7 @@
+
+
+Navbar examples. I used the 2nd one as its not very different other than the CSS
+https://foolishdeveloper.com/navigation-bar-html-css-javascript/
+
+Hiding an element in Javascript. Used this on the navbar
+https://www.scaler.com/topics/javascript-hide-element/
--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,17 +1,19 @@
-const main = document.querySelector('main');
 
-const section = document.createElement('section');
-section.classList.add('hello-container');
-main.appendChild(section);
+document.addEventListener('DOMContentLoaded', () => {
 
-const img = document.createElement('img');
-img.classList.add('img');
-img.setAttribute('src', '../images/chrissloggett.png');
-img.setAttribute('width', '500');
-img.setAttribute('alt', 'Photo of Christopher Sloggett');
-section.appendChild(img);
+    const currentPath = window.location.pathname;
+    const navbarPath = currentPath.includes('/contact/') ? '../navbar/navbar.html' : 'navbar/navbar.html';
 
-const p = document.createElement('p');
-p.classList.add('bio');
-p.textContent = `My name is Christopher Sloggett. I am a Senior student at Portland State University, majoring in Computer Science. Having been interested in web development for quite some time I am very excited to be a part of this class! In my off time, I enjoy suring, sailing, snowboarding, offroading, climbing, hiking, computers, and many many more things!`;
-section.appendChild(p);
+    fetch('navbar/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+
+        const homeLink = document.getElementById('home-link');
+        const currentPage = window.location.pathname;
+
+        if (currentPage.endsWith('index.html') || currentPage === '/') {
+            homeLink.style.display = 'none';
+        }
+    }).catch(error => console.error('Error loading navbar: ', error));
+});
