21a3167bb9f7d5fc46aa07dcb1003bbd10079205
Christopher Sloggett <mrsloggett@gmail.com>
2024-11-24 15:40:59 -0800

    Got the navbar to work on every page, making sure that the paths updated correctly for each url. Also added a coming soon for parts that werent finished

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,19 +1,30 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+  const currentPath = window.location.pathname;
+  let navbarPath;
 
-    const currentPath = window.location.pathname;
-    const navbarPath = currentPath.includes('/contact/') ? '../navbar/navbar.html' : 'navbar/navbar.html';
+  if (currentPath.includes('/about') || currentPath.includes('/contact') || currentPath.includes('/projects') || currentPath.includes('/previous-work')) {
+    navbarPath = '../navbar/navbar.html';
+  } else {
+    navbarPath = 'navbar/navbar.html';
+  }
 
-    fetch('navbar/navbar.html')
+  fetch(navbarPath)
     .then(response => response.text())
     .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
+      const navbarPlaceholder = document.getElementById('navbar-placeholder');
+      if (navbarPlaceholder) {
+        navbarPlaceholder.innerHTML = data;
 
         const homeLink = document.getElementById('home-link');
         const currentPage = window.location.pathname;
 
         if (currentPage.endsWith('index.html') || currentPage === '/') {
-            homeLink.style.display = 'none';
+          homeLink.style.display = 'none';
         }
-    }).catch(error => console.error('Error loading navbar: ', error));
+      } else {
+        console.error('Navbar placeholder element not found');
+      }
+    })
+    .catch(error => console.error('Error loading navbar:', error));
 });
